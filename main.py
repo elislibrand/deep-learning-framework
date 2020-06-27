0ec74bcf3efe902056cae58a010656d4b2288e13
elislibrand <elis.librand@gmail.com>
2020-06-28 00:09:20 +0200

    Added normalization of inputs

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ inputs = np.array([[0, 0, 0],
                    [0, 0, 255],
                    [0, 255, 0]])
 
-processed_inputs = np.array([[0, 0, 0],
-                             [1, 1, 1],
-                             [1, 1, 0],
-                             [0, 0, 1],
-                             [0, 1, 0]])
+inputs_norm = (inputs / 255)
 
 targets = np.array([[1],
                     [0],
@@ -25,4 +21,4 @@ model = Sequential()
 model.add(Dense(n_inputs = 3, n_neurons = 3, activation = 'relu'))
 model.add(Dense(n_inputs = 3, n_neurons = 1, activation = 'sigmoid'))
 
-model.fit(processed_inputs, targets, eta = 0.05, n_epochs = 50000)
+model.fit(inputs_norm, targets, eta = 0.05, n_epochs = 50000)
