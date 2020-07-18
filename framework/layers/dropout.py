89ac4fbead0a54b41bc5076ea276a482fe74f66d
elislibrand <elis.librand@yahoo.com>
2020-07-18 23:30:42 +0200

    Simplified Dropout layer (still not working)

--- framework/layers/dropout.py
+++ framework/layers/dropout.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# NOT WORKING PROPERLY
 class Dropout:
     def __init__(self, n_neurons, rate):
         self.n_neurons = n_neurons
@@ -9,9 +8,6 @@ class Dropout:
         
     def activate(self, inputs):
         self.inputs = inputs
-        
-        self.weights = np.ones((self.n_neurons, self.n_neurons))
-        self.biases = np.zeros((self.n_neurons, self.n_neurons))
         
         self.dropout = np.zeros(self.n_neurons, dtype = bool)
         self.dropout[np.where(np.random.uniform(size = self.n_neurons) < self.rate)] = True
@@ -24,9 +20,7 @@ class Dropout:
         return self.outputs
     
     def differentiate(self, inputs):
-        inputs[np.where(inputs > 0)] = 1
-        
-        return inputs
+        pass
     
     def regularize(self, eta):
         pass
