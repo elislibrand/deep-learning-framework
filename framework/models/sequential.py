Implementation of binary cross-entropy loss, correcting an error in the backpropagation

framework/models/sequential.py
@@ -26,8 +26,9 @@ class Sequential:
                 
         self.is_built = True
         
-    def compile(self, optimizer = SGD()):
+    def compile(self, optimizer = SGD(), loss = None):
         self.optimizer = optimizer
+        self.loss = loss
         
     @staticmethod
     def shuffle(inputs, targets):
@@ -52,7 +53,7 @@ class Sequential:
             layer = self.layers[i]
             
             if layer == self.layers[-1]:
-                layer.errors = targets - outputs # Change to: outputs - targets
+                layer.errors = self.loss.differentiate(targets, outputs)
             else:
                 next_layer = self.layers[i + 1]
                 
@@ -69,7 +70,7 @@ class Sequential:
     
     def fit(self, inputs, targets, batch_size, n_epochs, shuffle = True, seed = None):
         self.build(inputs.shape[1], seed)
-                        
+        
         for i in range(n_epochs):
             if shuffle:
                 inputs, targets = self.shuffle(inputs, targets)
@@ -80,10 +81,10 @@ class Sequential:
                 outputs_batch = self.forward(inputs_batch)
                 self.backward(targets_batch, outputs_batch)
                         
-            print('Epoch {:<{width}}    [accuracy: {:.3f} - loss: {:.3f}]'.format((i + 1),
-                                                                   np.around(self.get_accuracy(targets, self.forward(inputs)), 3),
-                                                                   np.around(np.mean(-(targets * np.log(self.forward(inputs)) + (1 - targets) * np.log(1 - self.forward(inputs)))), 3),
-                                                                   width = len(str(n_epochs))))
+            print('Epoch {:<{width}}    [accuracy: {:.3f}    loss: {:.3f}]'.format((i + 1),
+                                                                                   np.around(self.get_accuracy(targets, self.forward(inputs)), 3),
+                                                                                   np.around(self.loss.calculate(targets, self.forward(inputs)), 3),
+                                                                                   width = len(str(n_epochs))))
             
     def predict(self, inputs):
         predictions = self.forward(inputs)

framework/optimizers/sgd.py
@@ -13,8 +13,8 @@ class SGD:
         adjustment_weights = self.learning_rate * layer.gradients + momentum_weights
         adjustment_biases = self.learning_rate * np.sum(layer.deltas, axis = 0, keepdims = True) + momentum_biases
         
-        layer.weights += adjustment_weights
-        layer.biases += adjustment_biases
+        layer.weights -= adjustment_weights
+        layer.biases -= adjustment_biases
         
         layer.previous_adjustment_weights = adjustment_weights
         layer.previous_adjustment_biases = adjustment_biases

framework/regularizers/l1.py
@@ -6,4 +6,4 @@ class L1(Regularizer):
         self.amount = amount
         
     def regularize(self, layer):
-        layer.gradients -= np.sign(layer.weights) * self.amount
+        layer.gradients += np.sign(layer.weights) * self.amount

main.py
@@ -2,8 +2,9 @@ import numpy as np
 from framework.datasets import rgb
 from framework.models import Sequential
 from framework.layers import Dense
-from framework.activations import ReLU, Sigmoid
 from framework.optimizers import SGD
+from framework.activations import ReLU, Sigmoid
+from framework.losses import BinaryCrossEntropy
 from framework.initializers import GlorotNormal, HeUniform
 from framework.regularizers import L1, L2
 from framework.constraints import MaxNorm
@@ -36,9 +37,9 @@ def main():
         Dense(1, activation = Sigmoid(), initializer = HeUniform(), regularizers = [L1(amount = 1e-4)], constraints = [MaxNorm()])
     ])
     
-    model.compile(optimizer = SGD(learning_rate = 0.01, momentum = 0.9))
+    model.compile(optimizer = SGD(learning_rate = 1e-4, momentum = 0.9), loss = BinaryCrossEntropy())
     
-    model.fit(inputs, targets, batch_size = 32, n_epochs = 100)
+    model.fit(inputs, targets, batch_size = 32, n_epochs = 1)
     
     # Prediction
     prediction = int(np.squeeze(model.predict(get_color())))